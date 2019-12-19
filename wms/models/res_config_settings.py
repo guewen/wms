7486fed5fc5ff39a146ee8651794b03be1e0a688
Guewen Baconnier <guewen.baconnier@camptocamp.com>
2019-12-19 16:13:43 +0100

    [IMP] wms: black, isort

--- wms/models/res_config_settings.py
+++ wms/models/res_config_settings.py
@@ -10,15 +10,9 @@ class ResConfigSettings(models.TransientModel):
     module_stock_vertical_lift_kardex = fields.Boolean(
         string="Vertical Lift Using Kardex"
     )
-    module_stock_putaway_rule = fields.Boolean(
-        string="Configurable Put-Away Rules"
-    )
-    module_stock_putaway_recursive = fields.Boolean(
-        string="Recursive Put-Away rules"
-    )
-    module_stock_putaway_abc = fields.Boolean(
-        string="ABC chaotic storage Put-Away"
-    )
+    module_stock_putaway_rule = fields.Boolean(string="Configurable Put-Away Rules")
+    module_stock_putaway_recursive = fields.Boolean(string="Recursive Put-Away rules")
+    module_stock_putaway_abc = fields.Boolean(string="ABC chaotic storage Put-Away")
     module_stock_move_location_dest_constraint_empty = fields.Boolean(
         string="Put-Away Constraint: Empty bin"
     )
